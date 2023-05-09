3772a5677c745d02bc549b748abdffb80c4dab67
Staqya98 <staqya.hussaini98@gmail.com>
2023-05-09 14:01:34 +0100

    Simple HA node server initialized

--- server.js
+++ server.js
@@ -1,35 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const SerialPort = require('serialport');
+const port = process.env.PORT || 3000;
 
-// Replace "COM4" with the correct serial port for your Pico
-const port = new SerialPort("COM4", { baudRate: 9600 });
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname)));
 
-port.on("open", () => {
-  console.log("Serial port opened");
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  
-  socket.on('set_rgb_led', (data) => {
-    console.log('set_rgb_led', data);
-    const message = `r${data.r}g${data.g}b${data.b}\n`;
-    port.write(message, (err) => {
-      if (err) {
-        console.log("Error writing to serial port:", err);
-        }
-      }); 
-    });
-  });
-});
-  
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
