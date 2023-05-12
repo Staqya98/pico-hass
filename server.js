7a69c88aeacc50790edaefbfc8162c68af68cf00
Staqya98 <staqya.hussaini98@gmail.com>
2023-05-12 09:20:08 +0100

    using websocket and webrepl to create connection

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const path = require('path');
-const SerialPort = require('serialport');
+const WebSocket = require('websocket').client;
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Connect to the Pico's serial port
-const picoSerialPort = 'COM4'; // Change this to the correct port for your system
-const pico = new SerialPort(picoSerialPort, { baudRate: 9600 });
+// Connect to the Pico using WebSocket
+const picoWebSocketUrl = 'ws://your-pico-ip-address:8266';
+const wsClient = new WebSocket();
+
+wsClient.on('connectFailed', (error) => {
+  console.error(`Connection Error: ${error.toString()}`);
+});
+
+wsClient.on('connect', (connection) => {
+  console.log('Connected to Pico WebSocket.');
+
+  connection.on('error', (error) => {
+    console.error(`Connection Error: ${error.toString()}`);
+  });
+
+  connection.on('close', () => {
+    console.log('Connection closed.');
+  });
+
+  app.post('/api/color', (req, res) => {
+    const { r, g, b } = req.body;
+    const command = `set_rgb_color(${r}, ${g}, ${b})\r\n`;
+    connection.sendUTF(command);
+    res.json({ success: true });
+  });
+});
+
+wsClient.connect(picoWebSocketUrl);
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname)));
@@ -14,13 +39,6 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.post('/api/color', (req, res) => {
-  const { r, g, b } = req.body;
-  const command = `set_rgb_color(${r}, ${g}, ${b})\r\n`;
-  pico.write(command);
-  res.json({ success: true });
 });
 
 app.listen(port, () => {
